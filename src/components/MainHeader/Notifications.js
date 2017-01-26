d5330d41ef933e03bc61a5dcdc2bb503204c21ac
David Lavieri <daviddlavier@gmail.com>
2017-01-26 09:53:16 -0400

    Add navbar Notifications menu

--- src/components/MainHeader/Notifications.js
+++ src/components/MainHeader/Notifications.js
@@ -3,15 +3,52 @@
 // License that can be found in the LICENSE file.
 
 import React from 'react'
-
+import {connect} from 'react-redux'
 import {MainHeaderComponents} from 'react-adm-lte'
+
+import actions from './../../store/mainHeader/actions'
 
 const {Notifications} = MainHeaderComponents
 
-const MyNotifications = () => {
+const outerProps = {
+  count: 5,
+  header: 'You have 5 notifications',
+  footer: 'Show All Notifications',
+  data: [{
+    id: 1,
+    title: '5 new members joined today',
+    iconClass: 'fa fa-users text-aqua'
+  }, {
+    id: 2,
+    title: 'Very long description here that may not be show',
+    iconClass: 'fa fa-warning text-yellow'
+  }, {
+    id: 3,
+    title: '5 new members joined',
+    iconClass: 'fa fa-users text-red'
+  }, {
+    id: 4,
+    title: '25 sales made',
+    iconClass: 'fa fa-shopping-cart text-green'
+  }, {
+    id: 5,
+    title: 'You changed your username',
+    iconClass: 'fa fa-user text-red'
+  }]
+}
+
+const MyNotifications = props => {
   return (
-    <Notifications data={[]} />
+    <Notifications {...outerProps} {...props} />
   )
 }
 
-export default MyNotifications
+const mapStateToProps = ({mainHeader: {notifications}}) => ({
+  open: notifications.open
+})
+
+const mapDispatchToProps = dispatch => ({
+  onToggle: open => dispatch(actions.toggleNotifications(open))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(MyNotifications)
