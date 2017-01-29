d0bf5d5a40a4a727d6aaaed5d38044b189b12399
David Lavieri <daviddlavier@gmail.com>
2017-01-29 18:55:17 -0400

    Add ControlSidebar tab selection

--- src/components/ControlSidebar.js
+++ src/components/ControlSidebar.js
@@ -4,31 +4,58 @@
 
 import React, {PropTypes} from 'react'
 import {connect} from 'react-redux'
-
 import {ControlSidebarComponents} from 'react-adm-lte'
+
+import actions from '../store/controlSidebar/actions'
 
 const {ControlSidebar, ControlSidebarBackground, NavTab, Tab} = ControlSidebarComponents
 
-const MyControlSidebar = ({open}) => {
+const MyControlSidebar = props => {
+  const {open, onClickTab} = props
+  const {wrenchActive, homeActive, gearsActive} = props
+
   return (
     <ControlSidebar open={open}>
       <NavTab>
-        <Tab active icon='fa fa-wrench' />
-        <Tab icon='fa fa-home' />
-        <Tab icon='fa fa-gears' />
+        <Tab
+          onClick={() => onClickTab('wrench')}
+          active={wrenchActive}
+          icon='fa fa-wrench' />
+        <Tab
+          onClick={() => onClickTab('home')}
+          active={homeActive}
+          icon='fa fa-home' />
+        <Tab
+          onClick={() => onClickTab('gears')}
+          active={gearsActive}
+          icon='fa fa-gears' />
       </NavTab>
     </ControlSidebar>
   )
 }
 
 MyControlSidebar.propTypes = {
-  open: PropTypes.bool
+  open: PropTypes.bool,
+  onClickTab: PropTypes.func.isRequired,
+  wrenchActive: PropTypes.bool,
+  homeActive: PropTypes.bool,
+  gearsActive: PropTypes.bool
 }
 
-const ControlSidebarContainer = connect(
-  ({controlSidebar: {open}}) => ({open})
+const mapStateToProps = ({controlSidebar}) => ({
+  open: controlSidebar.open,
+  wrenchActive: controlSidebar.wrench.active,
+  homeActive: controlSidebar.home.active,
+  gearsActive: controlSidebar.gears.active
+})
+
+const mapDispatchToProps = dispatch => ({
+  onClickTab: key => dispatch(actions.selectControlSidebarTab(key))
+})
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
 )(MyControlSidebar)
 
 export {ControlSidebarBackground}
-
-export default ControlSidebarContainer
