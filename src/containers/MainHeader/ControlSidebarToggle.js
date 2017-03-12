d8c93ab19e63fc0462653770d74f16394a22217d
David Lavieri <daviddlavier@gmail.com>
2017-03-12 13:32:48 -0400

    Remove unnecessary licence headers on files

--- src/containers/MainHeader/ControlSidebarToggle.js
+++ src/containers/MainHeader/ControlSidebarToggle.js
@@ -1,7 +1,3 @@
-// Copyright 2017 David Lavieri.  All rights reserved.
-// Use of this source code is governed by a MIT License
-// License that can be found in the LICENSE file.
-
 import {connect} from 'react-redux'
 import {MainHeaderComponents} from 'react-adm-lte'
 
